ef5304acf5a38d1d693b3e11940057de546d69a7
Jason Taylor <jason@redant.com.au>
2018-11-20 23:52:30 +1100

    Hashed names and manifest

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
 
   output: {
     path: `${__dirname}/assets/webpack`,
+    filename: process.env.NODE_ENV === 'production'
+      ? '[name].[chunkhash].js'
+      : '[name].js',
   },
 
   module: {
@@ -19,13 +22,13 @@ module.exports = {
     ]
   },
 
-  // plugins: [
-  //   // Reference: https://github.com/danethurber/webpack-manifest-plugin
-  //   // Output the manifest for jekyll to import
-  //   new ManifestPlugin({
-  //     fileName: `${__dirname}/_data/webpack.json`,
-  //     publicPath: 'assets/webpack/',
-  //     writeToFileEmit: true,
-  //   }),
-  // ]
+  plugins: [
+    // Reference: https://github.com/danethurber/webpack-manifest-plugin
+    // Output the manifest for jekyll to import
+    new ManifestPlugin({
+      fileName: `${__dirname}/_data/webpack.json`,
+      publicPath: 'assets/webpack/',
+      writeToFileEmit: true,
+    }),
+  ]
 };
